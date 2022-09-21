6b6a3fc8f1c534507b002251ae4c35de9197d9db
croat25 <sala4460@mylaurier.ca>
2022-09-20 20:07:59 -0400

    remove useless stuff

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import VueCollapsiblePanel from '@dafcoe/vue-collapsible-panel'
 
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(router)
-app.use(VueCollapsiblePanel)
 app.mount('#app')
